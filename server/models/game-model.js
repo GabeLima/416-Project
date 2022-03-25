e752d8e261ecfe0b6be4d56f2c391ede8384e48d
Tim Wu <timothy.wu@stonybrook.edu>
2022-03-25 14:09:03 -0400

    Update game-model.js

--- server/models/game-model.js
+++ server/models/game-model.js
@@ -12,16 +12,12 @@ const CommentSchema = new Schema(
 
 const GameSchema = new Schema(
     {
-        isLive: { type: Boolean, required: true },
         players: { type: [String], required: true },
         panels: { type: [String], required: true },
         playerVotes: { type: [String], required: true},
         communityVotes: { type: [String], required: true },
         gameID: {type: String, required: true},
         comments: { type: [CommentSchema], required: true },
-        rounds: { type: Number, required: true },
-        timePerRound: { type: Number, required: true },
-        isPublic: { type: Boolean, required: true },
         tags: { type: [String], required: true},
     },
     { timestamps: true }
