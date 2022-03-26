4505485336a48879bbc084d39f25032b0c79d97b
Vicky Chan <vicky.chan@stonybrook.edu>
2022-03-26 15:32:12 -0400

    Added to Router and UserController
    added routes related to game and user followers.
    added function for updating followers

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,7 +1,13 @@
 const auth = require('../auth')
 const express = require('express')
 const UserController = require('../controllers/user-controller')
+const GameController = require('../controllers/game-controller')
 const router = express.Router()
+
+router.get('/search/:query', auth.verify, GameController.search)
+router.get('/game/:gameID', auth.verify, GameController.getGame)
+router.put('/game/:gameID', auth.verify, GameController.updateGame)         //3 and 4 since they are the same thing (update vote/update comment)
+router.put('/game/:email/followers', auth.verify, UserController.updateFollowers)
 
 router.post('/register', UserController.registerUser)
 router.post('/user', UserController.loginUser)
