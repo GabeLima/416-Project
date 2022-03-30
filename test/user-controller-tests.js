76b45aa32e4cc6e9848a69448c83ba7c9dec7de8
Tim Wu <timothy.wu@stonybrook.edu>
2022-03-30 19:48:45 -0400

    Skeleton code for tests. We might want to change up the formatting of… (#25)
    * Skeleton code for tests. We might want to change up the formatting of it, not sure yet.
    
    * Updated package.json dependencies, as well as added a way of running tests. Also fixed up skeleton code for socket-tests.js and implemented tests for changePassword in user-controller-tests.js

--- test/user-controller-tests.js
+++ test/user-controller-tests.js
@@ -1,7 +1,64 @@
-// Gabe: updateUser, loginUser, registerUser, updateFollowers
+const chai = require('chai');
+const sinon = require("sinon");
+const mongoose = require("mongoose");
+const UserModel = require("../models/user-model");
+const UserController = require("../controllers/user-controller");
 
-// Tim: changePassword
+chai.should();
+let sandbox = sinon.createSandbox();
 
-// Vicky (leader): getLoggedIn, logoutUser, getUser
+describe("how the user controller deals with requests", () => {
+  let res = {};
+    beforeEach(() => {
+        sandbox = sinon.createSandbox();
+        res = {
+            json: sandbox.spy(),
+            status: sandbox.stub().returns({end: sandbox.spy()})
+        };
+    });
+  
+    afterEach(() => {
+        sandbox.restore();
+    });
+  
+   
+    // TODO - IMPLEMENT BELOW
+    // Tim: changePassword
+    it("changes a password", (done) => {
+        let req = {
+                body: {
+                    email: "test@gmail.com",
+                    password: "oldpassword",
+                    newPassword: "newpassword",
+                    newPasswordVerify: "newpassword"
+                }
+        };
 
-// David: resetPassword
+        sandbox.stub(mongoose.Model, "findOne").yields(null);
+
+        UserController.changePassword(req, res);
+
+        sinon.assert.calledWith(UserModel.findOne, { email: 'test@gmail.com' });
+
+        done();
+    });
+
+
+    // Vicky (leader): getLoggedIn, logoutUser, getUser
+    it("gets whether a user is logged in", () => {
+  
+    });
+
+    it("logs out a user", () => {
+  
+    });
+
+    it("gets a user", () => {
+  
+    });
+
+    // David: resetPassword
+    it("resets a password", () => {
+  
+    });
+});
