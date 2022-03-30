e614e417592a65a9f16a070a8da7b0bd9d45db21
GabeLima <71938235+GabeLima@users.noreply.github.com>
2022-03-30 12:04:04 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
     origin: ["http://localhost:3000"],
-    credentials: false
+    credentials: true
 }))
 app.use(express.json())
 app.use(cookieParser())
