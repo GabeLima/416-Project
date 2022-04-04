f18f4590e3acc22a97fb673a688d57f9a2da65f1
GabeLima <71938235+GabeLima@users.noreply.github.com>
2022-04-03 23:18:24 -0400

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,15 +1,3 @@
-/*
-    This is our http api, which we use to send requests to
-    our back-end API. Note we`re using the Axios library
-    for doing this, which is an easy to use AJAX-based
-    library. We could (and maybe should) use Fetch, which
-    is a native (to browsers) standard, but Axios is easier
-    to use when sending JSON back and forth and it`s a Promise-
-    based API which helps a lot with asynchronous communication.
-    
-    @author McKilla Gorilla
-*/
-
 import axios from 'axios'
 axios.defaults.withCredentials = true;
 const api = axios.create({
