7efa65fa74a25f46f9052d5afb579b37c338298b
Tim Wu <timothy.wu@stonybrook.edu>
2022-04-08 00:03:34 -0400

    changed client/src/api/index.js to work for the heroku API instead of local build.

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 axios.defaults.withCredentials = true;
 const api = axios.create({
-    baseURL: 'http://localhost:4000/api' //LOCAL BUILD
-    //baseURL: 'https://derit.herokuapp.com/api', HEROKU DEPLOYMENT 
+    //baseURL: 'http://localhost:4000/api' //LOCAL BUILD
+    baseURL: 'https://derit.herokuapp.com/api'
 
 })
 export const getLoggedIn = () => api.get(`/loggedIn/`);
